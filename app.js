132d050a61b99707ad17cb617515d9696ceffa1b
Elizzy01 <elizabethogunmola01@gmail.com>
2024-08-19 11:16:40 +0100

    change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hola amigo'));
+app.get('/', (req, res) => res.send('Back to the grind!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
