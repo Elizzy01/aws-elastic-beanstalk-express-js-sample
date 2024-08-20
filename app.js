84a300b638f6ab3f1c503e9d5389025af62baf33
Elizzy01 <elizabethogunmola01@gmail.com>
2024-08-20 09:27:55 +0100

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Back to the grind!'));
+app.get('/', (req, res) => res.send('Back to the grind peeps!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
